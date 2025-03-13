618a739c1140d977fcc2ce3c9cf82a7f8db7581c
DavidSosa <davidgsosa1@gmail.com>
2025-03-13 15:46:00 -0700

    Added full funcitonaltiy to login and register form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -7,26 +7,38 @@ export default function LoginPage() {
 
     return (
         <>
-            <Box
-                width="100%"
-                backgroundColor={theme.palette.background.alt}
-                p="1rem 6%"
-                textAlign="center"
-            >
-                <Typography fontWeight="bold" fontSize="32px" color="primary">
-                    My Social
-                </Typography>
-            </Box>
-            <Box
-                width={isNonMobileScreens ? "50%" : "93%"}
-                p="2rem"
-                m="2rem auto"
-                borderRadius="1.5rem"
-                backgroundColor={theme.palette.background.alt}
-            >
-                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-                    Welcome to my social
-                </Typography>
+            <Box>
+                <Box
+                    width="100%"
+                    backgroundColor={theme.palette.background.alt}
+                    p="1rem 6%"
+                    textAlign="center"
+                >
+                    <Typography
+                        fontWeight="bold"
+                        fontSize="32px"
+                        color="primary"
+                    >
+                        My Social
+                    </Typography>
+                </Box>
+
+                <Box
+                    width={isNonMobileScreens ? "50%" : "93%"}
+                    p="2rem"
+                    m="2rem auto"
+                    borderRadius="1.5rem"
+                    backgroundColor={theme.palette.background.alt}
+                >
+                    <Typography
+                        fontWeight="500"
+                        variant="h5"
+                        sx={{ mb: "1.5rem" }}
+                    >
+                        Welcome to My Social
+                    </Typography>
+                    <Form />
+                </Box>
             </Box>
         </>
     );
