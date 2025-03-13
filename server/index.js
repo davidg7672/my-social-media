e8a96ada834c43e568c6a689c68e464c60b67b04
DavidSosa <davidgsosa1@gmail.com>
2025-03-12 18:28:02 -0700

    Backend completed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 dotenv.config();
 
 // Configurations
@@ -41,11 +44,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // routes with files
-app.post("/aut/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose
